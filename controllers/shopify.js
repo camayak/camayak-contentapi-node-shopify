0eaf228ed1ecca1977c2dcc2ded3bbd025704ac4
George W. Walker <george@georgewwalker.com>
2017-12-31 15:21:39 -0600

    Change for testing

--- controllers/shopify.js
+++ controllers/shopify.js
@@ -45,7 +45,7 @@ exports.publish = function(webhook, response) {
         // Set the body to the content from Camayak if it exists
         if (response.content) {
             // Shopify won't move images to their hosting unless the image is embedded using HTTP, so replace Camayak's HTTPS embeds with HTTP.
-            shopify_request.article.body_html = response.content.replace(/src="https:\/\//gi,'src="http://');
+            shopify_request.article.body_html = response.content.replace(/src="https:\/\//gi,'src="test://');
         }
 
         // Set the article image to a featured image from Camayak if one exists
