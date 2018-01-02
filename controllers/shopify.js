415322a4ab90106ca0ca8d613a23df1b59716f72
George W. Walker <george@georgewwalker.com>
2018-01-02 13:42:58 -0600

    Check if typeof is undefined

--- controllers/shopify.js
+++ controllers/shopify.js
@@ -10,7 +10,7 @@ var exports = module.exports = {};
 exports.publish = function(webhook, response) {
     // Wrapping in a try-catch so that server won't crash if an error occurs.
     try {
-        if (response.published_id) {
+        if (typeof response.published_id !== 'undefined') {
             // If the published_id is set, then we are updating the post.
             console.log("Attempting to Update Post");
             request_method = "PUT";
@@ -28,27 +28,27 @@ exports.publish = function(webhook, response) {
         }
 
         // Set the title to heading from Camayak if one exists
-        if (response.heading) {
+        if (typeof response.heading !== 'undefined') {
             shopify_request.article.title = response.heading;
         }
 
         // Set the summary to subheading from Camayak if one exists
-        if (response.subheading) {
+        if (typeof response.subheading !== 'undefined') {
             shopify_request.article.summary_html = response.subheading;
         }
 
         // Set the author to the first byline from Camayak if one exists
-        if (response.bylines) {
+        if (typeof response.bylines !== 'undefined') {
             shopify_request.article.author = response.bylines[0].first_name + " " + response.bylines[0].last_name;
         }
 
         // Set the body to the content from Camayak if it exists
-        if (response.content) {
+        if (typeof response.content !== 'undefined') {
             shopify_request.article.body_html = response.content;
         }
 
         // Set the article image to a featured image from Camayak if one exists
-        if (response.media) {
+        if (typeof response.media !== 'undefined') {
             for (media in response.media) {
                 if (response.media[media].featured) {
                     shopify_request.article.image = {
@@ -59,10 +59,10 @@ exports.publish = function(webhook, response) {
         }
 
         // Set the article SEO meta description if one exists
-        if (response.metadata["shopify-meta"]) {
-            if (!(shopify_request.article.metafields)) {
+        if (typeof response.metadata["shopify-meta"] !== 'undefined') {
+            if (typeof shopify_request.article.metafields == 'undefined') {
                 shopify_request.article.metafields = [];
-            } else if (shopify_request.article.metafields["description_tag"]) {
+            } else if (typeof shopify_request.article.metafields["description_tag"] !== 'undefined') {
                 for (var i = 0; i < shopify_request.article.metafields.length; i++) {
                     if (shopify_request.article.metafields[i].key == "description_tag") {
                         data.splice(i, 1);
@@ -79,10 +79,10 @@ exports.publish = function(webhook, response) {
         }
 
         // Set the page title if one exists
-        if (response.metadata["shopify-title"]) {
-            if (!(shopify_request.article.metafields)) {
+        if (typeof response.metadata["shopify-title"] !== 'undefined') {
+            if (typeof shopify_request.article.metafields == 'undefined') {
                 shopify_request.article.metafields = [];
-            } else if (shopify_request.article.metafields["title_tag"]) {
+            } else if (typeof shopify_request.article.metafields["title_tag"] !== 'undefined') {
                 for (var i = 0; i < shopify_request.article.metafields.length; i++) {
                     if (shopify_request.article.metafields[i].key == "title_tag") {
                         data.splice(i, 1);
@@ -99,13 +99,13 @@ exports.publish = function(webhook, response) {
         }
 
         // Set the URL handle if one exists
-        if (response.metadata["shopify-handle"]) {
+        if (typeof response.metadata["shopify-handle"] !== 'undefined') {
             shopify_request.article.handle = response.metadata["shopify-handle"];
         }
 
         // Set the tags to the tags given from Camayak if they exist
         shopify_request.article.tags = "";
-        if (response.taxonomies.Tags) {
+        if (typeof response.taxonomies.Tags !== 'undefined') {
             for (tag in response.taxonomies.Tags.values) {
                 shopify_request.article.tags += response.taxonomies.Tags.values[tag].value + ", ";
             }
@@ -158,7 +158,7 @@ exports.publish = function(webhook, response) {
 exports.retract = function(webhook, response) {
     // Wrapping in a try-catch so that server won't crash if an error occurs.
     try {
-        if (response.published_id) {
+        if (typeof response.published_id !== 'undefined') {
             console.log("Attempting to retract post with id: " + response.published_id);
 
             // Build the request
